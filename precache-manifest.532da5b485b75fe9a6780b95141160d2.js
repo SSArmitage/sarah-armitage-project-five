38c4ec526be4fecc42813729f499d058244f36e0
Sarah Armitage <saraharmitage314@gmail.com>
2019-11-30 19:14:35 -0500

    Updates

---
+++ precache-manifest.532da5b485b75fe9a6780b95141160d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b8e9d03b92e55dae8ede1453b685c2",
+    "url": "/sarah-armitage-project-five/index.html"
+  },
+  {
+    "revision": "64cb80b05e3584dfdf6e",
+    "url": "/sarah-armitage-project-five/static/css/main.e3df0678.chunk.css"
+  },
+  {
+    "revision": "8535048eb261e6bde8fa",
+    "url": "/sarah-armitage-project-five/static/js/2.6781fa2c.chunk.js"
+  },
+  {
+    "revision": "64cb80b05e3584dfdf6e",
+    "url": "/sarah-armitage-project-five/static/js/main.525c7a34.chunk.js"
+  },
+  {
+    "revision": "48aaf055ef22cdb239d9",
+    "url": "/sarah-armitage-project-five/static/js/runtime-main.b317e37b.js"
+  }
+]);
