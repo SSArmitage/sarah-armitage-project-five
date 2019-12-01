1d7f70c9e68e5f096c29c4ec0ef8dc8ee7010fb8
Sarah Armitage <saraharmitage314@gmail.com>
2019-11-30 19:23:51 -0500

    added fieldset and legend to login forms

--- src/SignInLogIn.js
+++ src/SignInLogIn.js
@@ -7,40 +7,46 @@ class SignInLogIn extends Component {
                 <div className="notSingedInPage">
                     <h2>Not signed in</h2>
                     {/* Sign up user */}
-                    {/* <form className="signUp"
+                    <form className="signUp"
                         onSubmit={this.props.onButtonClickSignUp}>
-                        <label htmlFor="email">Email Address</label>
-                        <input 
-                            type="email" 
-                            id="email" 
-                            placeholder="Email address"
-                            onChange={this.props.emailSignUp}/>
-                        <label htmlFor="password">Password</label>
-                        <input 
-                            type="password"
-                            id="password"
-                            placeholder="Password"
-                            onChange={this.props.passwordSignUp} />
-                        <button>Sign Up!</button>
-                    </form> */}
+                        <fieldset>
+                            <legend>Sign Up!</legend>
+                            <label htmlFor="email">Email Address</label>
+                            <input 
+                                type="email" 
+                                id="email" 
+                                placeholder="Email address"
+                                onChange={this.props.emailSignUp}/>
+                            <label htmlFor="password">Password</label>
+                            <input 
+                                type="password"
+                                id="password"
+                                placeholder="Password"
+                                onChange={this.props.passwordSignUp} />
+                            <button>Sign Up!</button>
+                        </fieldset>
+                    </form>
 
                     {/* Sign in user */}
                     <form 
                         className="login"
                         onSubmit={this.props.onButtonClickSignIn}>
-                        <label htmlFor="email">Email Address</label>
-                        <input
-                            type="email"
-                            id="email"
-                            placeholder="Email address"
-                            onChange={this.props.emailSignIn} />
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            placeholder="Password"
-                            onChange={this.props.passwordSignIn} />
-                        <button>Sign In</button>
+                        <fieldset>
+                            <legend>Sign In!</legend>
+                            <label htmlFor="email">Email Address</label>
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder="Email address"
+                                onChange={this.props.emailSignIn} />
+                            <label htmlFor="password">Password</label>
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="Password"
+                                onChange={this.props.passwordSignIn} />
+                            <button>Sign In</button>
+                        </fieldset>
                     </form>
                 </div>
             </div> 
